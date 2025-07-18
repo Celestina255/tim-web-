94edfb45672b942da94540c9e59637fbed7d1b87
Marcelino Kabuhung <147982311+MarcelinoKabuhung@users.noreply.github.com>
2025-07-18 16:20:08 +0700

    Update berita.php

--- dashboard/views/berita.php
+++ dashboard/views/berita.php
@@ -1,76 +1,86 @@
-      <section class="main-container" id="main-container">
-         <div class="container">
-            <div class="row">
-               <div class="col-lg-8">
-                  <?php 
-                  $query = mysqli_query ($con, "SELECT * FROM tb_berita JOIN tb_kategori ON tb_kategori.id_kategori=tb_berita.kategori ORDER BY tb_berita.id_berita DESC LIMIT 6");
-                  while($r=mysqli_fetch_array($query)){
-                     ?>
 
-                     <div class="post news-post">
-                        <div class="post-media post-image">
-                           <img class="img-fluid" src="../img/berita/<?php echo $r['gambar'];?>" alt="">
-                        </div>
-                        <div class="post-body clearfix">
-                           <!-- Post meta left-->
-                           <div class="post-content">
-                              <div class="entry-header">
-                                 <div class="post-meta"><span class="post-cat"><i class="icon icon-folder"></i><a href="#"> <?php echo $r['kategori'];?></a></span>
-                                    <span class="post-tag"><i class="icon icon-tag"></i><a href="#"> <?php echo $r['tgl_posting'];?></a></span>
-                                 </div>
-                                 <h2 class="entry-title"><a href="?page=detail_berita&slug=<?php echo $r['slug'];?>"><?php echo $r['judul'];?></a></h2>
-                              </div>
-                              <!-- header end-->
-                              <div class="entry-content">
-                                 <p><?php echo substr($r['isi'], 0, 300) ;?>..
-                                 </p>
-                              </div>
-                              <div class="post-footer "><a class="btn btn-primary" href="?page=detail_berita&slug=<?php echo $r['slug'];?>">Read More ...</a></div>
-                           </div>
-                           <!-- Post content right-->
-                        </div>
-                        <!-- post-body end-->
-                     </div>
-                  <?php } ?>
-               </div>
+<?php
+include '../koneksi.php';
 
+$limit = 6;
+$page = isset($_GET['hal']) ? (int)$_GET['hal'] : 1;
+$offset = ($page - 1) * $limit;
 
-               <!-- Content Col end-->
-               <div class="col-lg-4">
-                  <div class="sidebar sidebar-right">
-                     <div class="widget widget-search">
-                        <div class="input-group" id="search">
-                           <input class="form-control" placeholder="Search" type="search"><span class="input-group-btn"><i class="fa fa-search"></i></span>
-                        </div>
-                     </div>
-                     <div class="widget recent-posts">
-                        <h3 class="widget-title">Popular Posts</h3>
-                        <ul class="unstyled clearfix">
-                           <?php 
-                           $query2 = mysqli_query ($con, "SELECT * FROM tb_berita JOIN tb_kategori ON tb_kategori.id_kategori=tb_berita.kategori JOIN tb_admin ON tb_admin.id=tb_berita.user ORDER BY tb_berita.id_berita DESC LIMIT 6");
-                           while($rr=mysqli_fetch_array($query2)){
-                              ?>
-                              <li class="media">
-                                 <div class="media-left media-middle">
-                                    <img alt="img" src="../img/berita/kecil_<?php echo $rr['gambar'];?>">
-                                 </div>
-                                 <div class="media-body media-middle"><span class="post-date"><i class="icon icon-calendar-full"></i><a href="#"> <?php echo $rr['tgl_posting'];?></a></span>
-                                    <h4 class="entry-title"><a href="?page=detail_berita&slug=<?php echo $rr['slug'];?>"><?php echo $rr['judul'];?></a>
-                                       <small>by : <b><?php echo $rr['uname'];?></b></small>
-                                    </h4>
-                                 </div>
-                                 <div class="clearfix"></div>
-                              </li>
-                           <?php } ?>
-                        </ul>
-                     </div>
-                     <!-- Recent post end-->
+$query = mysqli_query($con, "SELECT * FROM tb_berita ORDER BY id_berita DESC LIMIT $limit OFFSET $offset");
+?>
+
+<style>
+    .btn-green {
+        background-color: #006633;
+        color: #fff;
+        border: none;
+    }
+    .btn-green:hover {
+        background-color: #004d26;
+        color: #fff;
+    }
+    .pagination .page-link {
+        color: #006633;
+        border: 1px solid #006633;
+    }
+    .pagination .page-link:hover {
+        background-color: #006633;
+        color: white;
+    }
+    .pagination .active .page-link {
+        background-color: #006633;
+        color: white;
+        border-color: #006633;
+    }
+    .pagination-container {
+        margin-bottom: 80px; /* jarak dari footer */
+    }
+</style>
+
+<div class="container mt-4">
+   <div class="row">
+      <?php while($r = mysqli_fetch_array($query)) { ?>
+         <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+               <img src="../img/berita/<?php echo $r['gambar']; ?>" class="bd-placeholder-img card-img-top" height="180">
+               <div class="card-body">
+                  <h5 class="card-title"><?php echo $r['judul']; ?></h5>
+                  <p class="card-text"><?php echo substr(strip_tags($r['isi']), 0, 100); ?>...</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                     <a href="?page=detail_berita&slug=<?php echo $r['slug']; ?>" class="btn btn-sm btn-green">Read More</a>
+                     <small class="text-muted"><?php echo $r['tgl_posting']; ?></small>
                   </div>
-                  <!-- Sidebar end-->
                </div>
-               <!-- Sidebar Col end-->
             </div>
-            <!-- Main row end-->
          </div>
-         <!-- Container end-->
-      </section>
+      <?php } ?>
+   </div>
+
+   <!-- Pagination -->
+   <div class="row pagination-container">
+      <div class="col-12 text-center">
+         <nav>
+            <ul class="pagination justify-content-center">
+               <?php
+               $result = mysqli_query($con, "SELECT COUNT(*) as total FROM tb_berita");
+               $total_data = mysqli_fetch_assoc($result)['total'];
+               $total_page = ceil($total_data / $limit);
+
+               if ($page > 1) {
+                  echo '<li class="page-item"><a class="page-link" href="?page=berita&hal=' . ($page - 1) . '">«</a></li>';
+               }
+
+               for ($i = 1; $i <= $total_page; $i++) {
+                  $active = ($i == $page) ? 'active' : '';
+                  echo '<li class="page-item ' . $active . '"><a class="page-link" href="?page=berita&hal=' . $i . '">' . $i . '</a></li>';
+               }
+
+               if ($page < $total_page) {
+                  echo '<li class="page-item"><a class="page-link" href="?page=berita&hal=' . ($page + 1) . '">»</a></li>';
+               }
+               ?>
+            </ul>
+         </nav>
+      </div>
+   </div>
+</div>
