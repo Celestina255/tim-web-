594bc1bece2ecc8856cd8439d561c7e4435fc316
Celestina255 <fmariwy@gmail.com>
2025-07-26 00:49:56 +0900

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -36,13 +36,13 @@ error_reporting(0);
    <!-- Owl Carousel-->
    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
    
-   <link rel="stylesheet" href="css/owl.carousel.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
    <!-- Owl Theme -->
-   <link rel="stylesheet" href="css/owl.theme.default.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
    <!-- Colorbox-->
    <link rel="stylesheet" href="css/colorbox.css">
    <!-- Template styles-->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="../dashboard/css/style.css">
    <!-- Responsive styles-->
    <link rel="stylesheet" href="css/responsive.css">
    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file.-->
@@ -59,7 +59,7 @@ error_reporting(0);
                ?>
               
          <?php } ?>
-<link rel="stylesheet" href="../dashboard/css/style.css">
+
 <!-- ini adalah tempat dimana file cssnya harus diupdate, jangan ditambah jika sudah ada -->
 
 <!-- Letakkan navbar setelah div class="body-inner yang ada isi file php -->
@@ -145,99 +145,80 @@ error_reporting(0);
       $queryy = mysqli_query ($con, "SELECT * FROM tb_kelurahan LIMIT 1");
       while ($rr = mysqli_fetch_array($queryy)){
    ?>
-   <footer class="footer" id="footer">
-      <div class="footer-main bg-overlay">
-         <div class="container">
-            <div class="row">
-               <div class="col-lg-4 col-md-12 footer-widget footer-about">
-                  <div class="footer-logo">
-                     <img src="../img/<?php echo $rr['logo']; ?>" style="width: 35px; margin-top: -20px;" alt="">
-                     <span style="font-size: 40px; text-shadow: 2px 2px 6px; margin-top: 10px; color: green;">Web-</span>
-                           <span style="font-size: 40px; text-shadow: 2px 2px 6px; margin-top: 10px; color: white;"><?php echo $rr['kelurahan']; ?></span>
-                  </div>
-                  <p>Desa Dagelan merupakan salah satu desa yang terletak di Kecamatan Way Asalan, dan merupakan desa yang unik dan menjadi bahan tertawaan warga setempat maupun warga sekitar Kecamatan Way Asalan.</p>
+   <!-- Footer start-->
+<footer class="footer" id="footer">
+  <div class="container">
+    <div class="row">
 
-                  <h3 class="widget-title">Subscribe!</h3>
-                  <form class="newsletter-form" id="newsletter-form" action="#" method="post">
-                     <div class="form-group">
-                        <input class="form-control form-control-lg" id="newsletter-form-email" type="email" name="email" placeholder="Email "
-                        autocomplete="off">
-                        <button class="btn btn-primary"><i class="fa fa-paper-plane"></i></button>
-                     </div>
-                  </form>
-               </div>
-               <!-- About us end-->
-               <div class="col-lg-4 col-md-6 footer-widget">
-                  <h3 class="widget-title">Quick Links</h3>
-                  <ul class="list-dash">
-                    <li><a href="?page=sejarah">Sejarah</a></li>
-                    <li><a href="?page=struktur">Struktur</a></li>
-                    <li><a href="?page=visismisi">Visi & Misi</a></li>
-                    <li><a href="?page=petadesa">Peta Desa</a></li>
-                    <li><a href="?page=tupoksi">Tupoksi</a></li>
-                    <li><a href="?page=aparat">Aparat Desa</a></li>
-                    <li><a href="?page=layanan">Pelayanan</a></li>
-                    <li><a href="?page=berita">Berita Desa Terbaru</a></li>
-                    <li><a href="?page=galeri">Galeri</a></li>
-                    <li><a href="?page=contact">Contact</a></li>
-                 </ul>
-              </div>
-              <div class="col-lg-4 col-md-6 footer-widget">
-               <h3 class="widget-title">Alamat</h3>
-               <div class="ts-contact-info"><span class="ts-contact-icon float-left"><i class="icon icon-map-marker2"></i></span>
-                  <div class="ts-contact-content">
-                     <h3 class="ts-contact-title">Kantor </h3>
-                     <p><?php echo $rr['kantor']; ?></p>
-                  </div>
-                  <!-- Contact content end-->
-               </div>
+      <!-- Kolom 1: Logo -->
+      <div class="col-lg-2 col-md-3 footer-widget text-center">
+        <img src="./img/<?php echo $rr['logo']; ?>" alt="Logo" class="footer-logo-img">
+      </div>
 
-               <div class="ts-contact-info last"><span class="ts-contact-icon float-left"><i class="icon icon-envelope"></i></span>
-                  <div class="ts-contact-content">
-                     <h3 class="ts-contact-title">Alamat E-mail</h3>
-                     <p><?php echo $rr['email']; ?></p>
-                  </div>
-                  <!-- Contact content end-->
-               </div>
-               <div class="ts-contact-info"><span class="ts-contact-icon float-left"><i class="icon icon-phone3"></i></span>
-                  <div class="ts-contact-content">
-                     <h3 class="ts-contact-title">Telp/Hp.</h3>
-                     <p><?php echo $rr['telp']; ?></p>
-                  </div>
-                  <!-- Contact content end-->
-               </div>
-            </div>
-         </div>
-         <!-- Content row end-->
+      <!-- Kolom 2: Teks dan alamat -->
+      <div class="col-lg-4 col-md-9 footer-widget-kampung">
+        <h3 class="footer-title">Pemerintah Kampung Banjar Ausoy</h3>
+        <p>Jalan Poros Manimeri Bintuni SP IV<br>
+          Kampung Banjar Ausoy, Kecamatan Manimeri,<br>
+          Kabupaten Teluk Bintuni<br>
+          Provinsi Papua Barat, 98364
+        </p>
       </div>
-      <!-- Container end-->
-   </div>
-   <!-- Footer Main-->
-   <div class="copyright">
-      <div class="container">
-         <div class="row">
-            <div class="col-lg-6 col-md-12">
-               <div class="copyright-info"><span>Copyright by <b>sdc</b> © 2023 a theme by <a href="https://furioustheme.com">Furioustheme</a></span></div>
-            </div>
-            <div class="col-lg-6 col-md-12">
-             <div class="footer-social text-right">
-               <ul>
-                  <li><a href="https://facebook.com"><i class="fa fa-facebook"></i></a></li>
-                  <li><a href="https://twitter.com"><i class="fa fa-twitter"></i></a></li>
-                  <li><a href="https://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
-                  <li><a href="https://github.com"><i class="fa fa-github"></i></a></li>
-                  <li><a href="https://instagram.com"><i class="fa fa-instagram"></i></a></li>
-               </ul>
-            </div>
-         </div>
+
+      <!-- Kolom 3: Hubungi Kami -->
+      <div class="col-lg-3 col-md-6 footer-widget-hubungi text-left">
+        <h3 class="footer-title">Hubungi Kami</h3>
+        <p><i class="fa fa-phone"></i> 082199656081</p>
+        <p><i class="fa fa-envelope"></i> banjarausoysp4@gmail.co.id</p>
+        <div class="social-icons">
+          <a href="#"><i class="fa fa-instagram"></i></a>
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-whatsapp"></i></a>
+          <a href="#"><i class="fa fa-youtube"></i></a>
+          <a href="#"><i class="fa fa-tiktok"></i></a>
+        </div>
       </div>
-      <!-- Row end-->
-   </div>
-   <!-- Container end-->
-</div>
-<!-- Copyright end-->
+
+      <!-- Kolom 4: Nomor penting -->
+      <div class="col-lg-2 col-md-6 footer-widget-tlp">
+        <h3 class="footer-title-tlp">Nomor Telepon Penting</h3>
+        <p>
+          <li><a href="?page=home">Sudirman/ Kades Banjar Ausoy</a></li>
+          <li><a href="?page=sejarah">Agus/ Sekdes Banjar Ausoy</a></li>
+          </p>
+      </div>
+
+      <!-- Kolom 5: Navigasi -->
+      <div class="col-lg-1 col-md-6 footer-widget-jelajahi">
+        <h3 class="footer-title">Jelajahi</h3>
+        <ul class="footer-links">
+          <li><a href="?page=home">Beranda</a></li>
+          <li><a href="?page=sejarah">Profil</a></li>
+          <li><a href="?page=layanan">Layanan</a></li>
+          <li><a href="?page=berita">Berita</a></li>
+          <li><a href="?page=bumdes">BUMDes</a></li>
+          <li><a href="?page=transparansi">Transparansi</a></li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- Copyright -->
+  <div class="footer-bottom">
+    <div class="container text-center">
+      <p>© 2025 - Website Kampung Banjar Ausoy Powered by KKN Tematik Unipa 2025</p>
+    </div>
+  </div>
+
+  <?php } ?>
+  <div class="back-to-top affix" id="back-to-top" data-spy="affix" data-offset-top="10">
+    <button class="btn btn-primary" title="Back to Top">
+      <i class="fa fa-angle-double-up"></i>
+    </button>
+  </div>
 </footer>
-<?php } ?>
+
 <!-- Footer end-->
 
       <div class="back-to-top affix" id="back-to-top" data-spy="affix" data-offset-top="10">
@@ -276,6 +257,38 @@ error_reporting(0);
    !function(e){e.fn.marquee=function(t){return this.each(function(){var i,a,n,r,s,o=e.extend({},e.fn.marquee.defaults,t),u=e(this),d=3,p="animation-play-state",f=!1,l=function(e,t,i){for(var a=["webkit","moz","MS","o",""],n=0;n<a.length;n++)a[n]||(t=t.toLowerCase()),e.addEventListener(a[n]+t,i,!1)},c=function(e){var t,i=[];for(t in e)e.hasOwnProperty(t)&&i.push(t+":"+e[t]);return i.push(),"{"+i.join(",")+"}"},m={pause:function(){f&&o.allowCss3Support?i.css(p,"paused"):e.fn.pause&&i.pause(),u.data("runningStatus","paused"),u.trigger("paused")},resume:function(){f&&o.allowCss3Support?i.css(p,"running"):e.fn.resume&&i.resume(),u.data("runningStatus","resumed"),u.trigger("resumed")},toggle:function(){m["resumed"==u.data("runningStatus")?"pause":"resume"]()},destroy:function(){clearTimeout(u.timer),u.css("visibility","hidden").html(u.find(".js-marquee:first")),setTimeout(function(){u.css("visibility","visible")},0)}};if("string"==typeof t)e.isFunction(m[t])&&(i||(i=u.find(".js-marquee-wrapper")),!0===u.data("css3AnimationIsSupported")&&(f=!0),m[t]());else{var g;e.each(o,function(e){if(g=u.attr("data-"+e),"undefined"!=typeof g){switch(g){case"true":g=!0;break;case"false":g=!1}o[e]=g}}),o.duration=o.speed||o.duration,r="up"==o.direction||"down"==o.direction,o.gap=o.duplicated?o.gap:0,u.wrapInner('<div class="js-marquee"></div>');var h=u.find(".js-marquee").css({"margin-right":o.gap,"float":"left"});if(o.duplicated&&h.clone(!0).appendTo(u),u.wrapInner('<div style="width:100000px" class="js-marquee-wrapper"></div>'),i=u.find(".js-marquee-wrapper"),r){var v=u.height();i.removeAttr("style"),u.height(v),u.find(".js-marquee").css({"float":"none","margin-bottom":o.gap,"margin-right":0}),o.duplicated&&u.find(".js-marquee:last").css({"margin-bottom":0});var y=u.find(".js-marquee:first").height()+o.gap;o.duration*=(parseInt(y,10)+parseInt(v,10))/parseInt(v,10)}else s=u.find(".js-marquee:first").width()+o.gap,a=u.width(),o.duration*=(parseInt(s,10)+parseInt(a,10))/parseInt(a,10);if(o.duplicated&&(o.duration/=2),o.allowCss3Support){var h=document.body||document.createElement("div"),w="marqueeAnimation-"+Math.floor(1e7*Math.random()),S=["Webkit","Moz","O","ms","Khtml"],x="animation",b="",q="";if(h.style.animation&&(q="@keyframes "+w+" ",f=!0),!1===f)for(var j=0;j<S.length;j++)if(void 0!==h.style[S[j]+"AnimationName"]){h="-"+S[j].toLowerCase()+"-",x=h+x,p=h+p,q="@"+h+"keyframes "+w+" ",f=!0;break}f&&(b=w+" "+o.duration/1e3+"s "+o.delayBeforeStart/1e3+"s infinite "+o.css3easing,u.data("css3AnimationIsSupported",!0))}var I=function(){i.css("margin-top","up"==o.direction?v+"px":"-"+y+"px")},C=function(){i.css("margin-left","left"==o.direction?a+"px":"-"+s+"px")};o.duplicated?(r?i.css("margin-top","up"==o.direction?v:"-"+(2*y-o.gap)+"px"):i.css("margin-left","left"==o.direction?a+"px":"-"+(2*s-o.gap)+"px"),d=1):r?I():C();var A=function(){if(o.duplicated&&(1===d?(o._originalDuration=o.duration,o.duration=r?"up"==o.direction?o.duration+v/(y/o.duration):2*o.duration:"left"==o.direction?o.duration+a/(s/o.duration):2*o.duration,b&&(b=w+" "+o.duration/1e3+"s "+o.delayBeforeStart/1e3+"s "+o.css3easing),d++):2===d&&(o.duration=o._originalDuration,b&&(w+="0",q=e.trim(q)+"0 ",b=w+" "+o.duration/1e3+"s 0s infinite "+o.css3easing),d++)),r?o.duplicated?(d>2&&i.css("margin-top","up"==o.direction?0:"-"+y+"px"),n={"margin-top":"up"==o.direction?"-"+y+"px":0}):(I(),n={"margin-top":"up"==o.direction?"-"+i.height()+"px":v+"px"}):o.duplicated?(d>2&&i.css("margin-left","left"==o.direction?0:"-"+s+"px"),n={"margin-left":"left"==o.direction?"-"+s+"px":0}):(C(),n={"margin-left":"left"==o.direction?"-"+s+"px":a+"px"}),u.trigger("beforeStarting"),f){i.css(x,b);var t=q+" { 100%  "+c(n)+"}",p=e("style");0!==p.length?p.filter(":last").append(t):e("head").append("<style>"+t+"</style>"),l(i[0],"AnimationIteration",function(){u.trigger("finished")}),l(i[0],"AnimationEnd",function(){A(),u.trigger("finished")})}else i.animate(n,o.duration,o.easing,function(){u.trigger("finished"),o.pauseOnCycle?u.timer=setTimeout(A,o.delayBeforeStart):A()});u.data("runningStatus","resumed")};u.bind("pause",m.pause),u.bind("resume",m.resume),o.pauseOnHover&&u.bind("mouseenter mouseleave",m.toggle),f&&o.allowCss3Support?A():u.timer=setTimeout(A,o.delayBeforeStart)}})},e.fn.marquee.defaults={allowCss3Support:!0,css3easing:"linear",easing:"linear",delayBeforeStart:1e3,direction:"left",duplicated:!1,duration:5e3,gap:20,pauseOnCycle:!1,pauseOnHover:!1}}(jQuery);
 $("#marquee").marquee({duration:1e4,gap:50,delayBeforeStart:10,direction:"left",duplicated:!0,pauseOnHover:!0});
 </script>
+<!-- Owl Carousel (di akhir sebelum </body>) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/owl.carousel@2.3.4/dist/owl.carousel.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/owl.carousel@2.3.4/dist/assets/owl.carousel.min.css">
+
+<script>
+$('#testimonial-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  responsive: {
+    0: { items: 1 },
+    576: { items: 2 },
+    992: { items: 3 }
+  }
+});
+
+
+  $('#prev').click(function() {
+    $('#testimonial-carousel').trigger('prev.owl.carousel');
+  });
+  $('#next').click(function() {
+    $('#testimonial-carousel').trigger('next.owl.carousel');
+  });
+</script>
+
+
+
+
 </body>
 
 </html>
