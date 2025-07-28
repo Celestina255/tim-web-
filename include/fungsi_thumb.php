411018ed3c36afbab98f89757e9af9264d4fa51f
Davina <135416325+desrbaa@users.noreply.github.com>
2025-07-28 11:08:10 +0900

    Update fungsi_thumb.php
    By Celin

--- include/fungsi_thumb.php
+++ include/fungsi_thumb.php
@@ -443,94 +443,17 @@ function UploadSlider($fupload_name){
 }
 
 // Upload gambar untuk galeri foto
-function UploadBerita($fupload_name){
-  //direktori gambar
-  $vdir_upload = "../img/berita/";
-  $vfile_upload = $vdir_upload . $fupload_name;
-  $imageType = $_FILES["file"]["type"];
+function UploadBerita($fupload_name) {
+    // Folder penyimpanan gambar
+    $vdir_upload = "../img/berita/";
+    $vfile_upload = $vdir_upload . $fupload_name;
 
-  //Simpan gambar dalam ukuran sebenarnya
-  move_uploaded_file($_FILES["file"]["tmp_name"], $vfile_upload);
+    // Pindahkan file asli ke folder berita
+    move_uploaded_file($_FILES["file"]["tmp_name"], $vfile_upload);
 
-  //identitas file asli
-  switch($imageType) {
-    case "image/gif":
-      $im_src=imagecreatefromgif($vfile_upload); 
-      break;
-      case "image/pjpeg":
-    case "image/jpeg":
-    case "image/jpg":
-      $im_src=imagecreatefromjpeg($vfile_upload); 
-      break;
-      case "image/png":
-    case "image/x-png":
-      $im_src=imagecreatefrompng($vfile_upload); 
-      break;
-  }
-  
-  $src_width = imageSX($im_src);
-  $src_height = imageSY($im_src);
-
-  //Simpan dalam versi besar 400 pixel
-  //Set ukuran gambar hasil perubahan
-  if($src_width>=550){
-  $dst_width = 550;
-  } else {
-  $dst_width = $src_width;
-  }
-  $dst_height = ($dst_width/$src_width)*$src_height;
-
-  //proses perubahan ukuran
-  $im = imagecreatetruecolor($dst_width,$dst_height);
-  imagecopyresampled($im, $im_src, 0, 0, 0, 0, $dst_width, $dst_height, $src_width, $src_height);
-   
-  //Simpan gambar
-  switch($imageType) {
-    case "image/gif":
-        imagegif($im,$vdir_upload.$fupload_name);
-      break;
-      case "image/pjpeg":
-    case "image/jpeg":
-    case "image/jpg":
-        imagejpeg($im,$vdir_upload.$fupload_name);
-      break;
-      case "image/png":
-    case "image/x-png":
-        imagepng($im,$vdir_upload.$fupload_name);
-      break;
-  }
-
-
-  //Simpan dalam versi small 200 pixel
-  //Set ukuran gambar hasil perubahan
-
-  $dst_width2 = 150;
-  $dst_height2 = ($dst_width2/$src_width)*$src_height;
-
-  //proses perubahan ukuran
-  $im2 = imagecreatetruecolor($dst_width2,$dst_height2);
-  imagecopyresampled($im2, $im_src, 0, 0, 0, 0, $dst_width2, $dst_height2, $src_width, $src_height);
-
-  //Simpan gambar
-  switch($imageType) {
-    case "image/gif":
-        imagegif($im2,$vdir_upload . "kecil_" . $fupload_name);
-      break;
-      case "image/pjpeg":
-    case "image/jpeg":
-    case "image/jpg":
-        imagejpeg($im2,$vdir_upload . "kecil_" . $fupload_name);
-      break;
-      case "image/png":
-    case "image/x-png":
-        imagepng($im2,$vdir_upload . "kecil_" . $fupload_name);
-      break;
-  }
-  
-  //Hapus gambar di memori komputer
-  imagedestroy($im_src);
-  imagedestroy($im);
-  imagedestroy($im2);
+    // Buat thumbnail 'palsu' (hanya copy file asli tanpa resize)
+    $thumb_file = $vdir_upload . "kecil_" . $fupload_name;
+    copy($vfile_upload, $thumb_file);
 }
 
 
