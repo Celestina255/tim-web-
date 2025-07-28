3b344e90abbf065e3ed98c3482211bcd90cb0050
LuciaSorontou <147905628+LuciaSorontou@users.noreply.github.com>
2025-07-28 22:56:46 +0700

    Update c_sukettanah.php

--- cetak/c_sukettanah.php
+++ cetak/c_sukettanah.php
@@ -6,9 +6,7 @@ include_once "../assets/inc.php";
 $kodesurat = $_GET['kode'];
 
 # Perintah untuk mendapatkan data dari tabel Surat 
-$query = mysqli_query ($con, "SELECT tb_jenissurat.*, tb_datasurat.*, tb_detailsurat.*, tb_penduduk.* 
-from tb_jenissurat, tb_datasurat, tb_detailsurat, tb_penduduk 
-WHERE tb_detailsurat.kode='$kodesurat' AND tb_detailsurat.nik=tb_penduduk.nik");
+$query = mysqli_query ($con, "SELECT * FROM tb_detailsurat JOIN tb_staff ON tb_detailsurat.ttd=tb_staff.id_staff LEFT JOIN tb_penduduk ON tb_detailsurat.nik=tb_penduduk.nik WHERE tb_detailsurat.kode='$kodesurat'");
 while ($r = mysqli_fetch_array($query)) {
   $dt = explode(';', $r['detail']);
   $tgl_sekarang = date('Y-m-d');
@@ -49,9 +47,8 @@ while ($r = mysqli_fetch_array($query)) {
     </td>
   </tr>
   <tr>
-    <td colspan="3" align="center"><hr style="border: 1.5px double black;"></td>
+    <td colspan="3" align="center"><hr style="border: 1.5px double black;"><br></td>
   </tr>
-
   <tr>
     <td colspan="3" align="center">
       <strong><u><?php echo strtoupper($r['nmsurat']); ?></u></strong><br>
@@ -62,61 +59,97 @@ while ($r = mysqli_fetch_array($query)) {
 <br>
 <table align="center" class="table-list" width="800" border="0" cellspacing="1" cellpadding="2">
   <tr>
-    <td colspan="4">Yang bertanda tangan dibawah ini 
-      <?php echo $rd['jnp']=='Desa'? "Kepala Kampung" : "Lurah";?> <?php echo $rd['kelurahan'];?> 
-      Distrik <?php echo $rd['kec'];?> Kabupaten <?php echo $rd['kab'];?>, dengan ini menerangkan sesungguhnya bahwa : </td>
+    <td colspan="4">Yang bertanda tangan dibawah ini <?php echo $rd['jnp']=='Desa'? "Kepala Kampung" : "Lurah";?> <?php echo $rd['kelurahan'];?> Distrik <?php echo $rd['kec'];?> Kabupaten <?php echo $rd['kab'];?>, Deangan ini menerangkan bahwa : </td>
   </tr>
-      <tr><td colspan="4">&nbsp;</td></tr>
-  <tr><td>1.</td><td>Nama</td><td>:</td><td><?php echo $r['nama'];?></td></tr>
-  <tr><td>2.</td><td>NIK</td><td>:</td><td><?php echo $r['nik'];?></td></tr>
-  <tr><td>3.</td><td>Jenis Kelamin</td><td>:</td><td><?php echo $r['jk'];?></td></tr>
-  <tr><td>4.</td><td>Tmp. & Tgl. Lahir</td><td>:</td><td><?php echo $r['tmp_lahir'];?>, <?php echo $r['tgl_lahir'];?></td></tr>
-  <tr><td>5.</td><td>Kewarganegaraan</td><td>:</td><td><?php echo $r['kwng'];?></td></tr>
-  <tr><td>6.</td><td>Agama</td><td>:</td><td><?php echo $r['agama'];?></td></tr>
-  <tr><td>7.</td><td>Pekerjaan</td><td>:</td><td><?php echo $r['kerjaan'];?></td></tr>
-  <tr><td>8.</td><td>Alamat</td><td>:</td>
-  <td><?php echo $r['alamat'];?> <?php echo $rd['jnp']=='Desa'? "Kampung" : "Kelurahan";?> <?php echo $r['kelurahan'];?></td></tr>
+      <tr>
+    <td colspan="4">&nbsp;</td>
+  </tr>
   <tr>
-    <td></td><td></td><td></td><td>Distrik <?php echo $r['kec'];?> Kabupaten <?php echo $r['kab'];?></td>
+    <td>1.</td><td>Nama</td><td>:</td><td><?php echo $r['nama'];?></td>
   </tr>
-
-  <tr><td colspan="4">&nbsp;</td></tr>
-  <tr><td colspan="4">Warga tersebut diatas memiliki Tanah yang diperoleh dari <?php echo $dt[4];?> seluas <?php echo format_angka($dt[2]);?> M2 <b><i>(<?php echo ucwords(terbilang($dt[2]));?> Meter Persegi)</i></b> yang terletak di <?php echo $dt[3];?>, dengan batas - batas sebagai berikut :</td>
-  </tr>
-  <tr><td>1.</td><td>Barat berbatasan dengan</td><td>:</td><td><?php echo $dt[5];?></td></tr>
-  <tr><td>2.</td><td>Utara berbatasan dengan</td><td>:</td><td><?php echo $dt[6];?></td></tr>
-  <tr><td>3.</td><td>Timur berbatasan dengan</td><td>:</td><td><?php echo $dt[7];?></td></tr>
-  <tr><td>4.</td><td>Selatan berbatasan dengan</td><td>:</td><td><?php echo $dt[8];?></td></tr>
-  <tr><td colspan="4">&nbsp;</td></tr>
-  <tr><td colspan="4">Demikian keterangan ini dibuat, untuk dapat dipergunakan sebagaimana mestinya. </td></tr>
-
-<tr><td colspan="4">
-<table width="100%" border="0" cellspacing="0" cellpadding="4" style="margin-top: 30px;">
   <tr>
-    <td width="50%"></td> <!-- Kolom kosong kiri -->
-    
-    <td width="50%" align="center">
-      <div style="font-size: 12pt; line-height: 1.5; text-align: center;">
-        Dikeluarkan di : <?php echo $rd['kelurahan']; ?><br>
-        Pada Tanggal &nbsp;&nbsp;: <?php echo tgl_indonesia($tgl_sekarang); ?>
-      </div>
-
-      <br>
-
-      <div style="font-weight: bold; font-size: 12pt;">
-        KEPALA KAMPUNG
-      </div>
-
-      <br><br><br>
-
-      <div style="text-align: center;">
-        <u><b><?php echo strtoupper($r['ttd']); ?></b></u><br>
-        NIP. <?php echo $rd['niplurah']; ?>
-      </div>
-    </td>
+    <td>2.</td><td>NIK</td><td>:</td><td><?php echo $r['nik'];?></td>
   </tr>
+    <tr>
+    <td>3.</td><td>Jenis Kelamin</td><td>:</td><td><?php echo $r['jk'];?></td>
+  </tr>
+      <tr>
+    <td>4.</td><td>Tmp. & Tgl. Lahir</td><td>:</td><td><?php echo $r['tmp_lahir'];?>, <?php echo $r['tgl_lahir'];?></td>
+  </tr>
+      <tr>
+    <td>5.</td><td>Kewarganegaraan</td><td>:</td><td><?php echo $r['kwng'];?></td>
+  </tr>
+    <tr>
+    <td>6.</td><td>Agama</td><td>:</td><td><?php echo $r['agama'];?></td>
+  </tr>
+      <tr>
+    <td>7.</td><td>Pekerjaan</td><td>:</td><td><?php echo $r['kerjaan'];?></td>
+  </tr>
+    <tr>
+    <td>8.</td><td>Alamat</td><td>:</td><td><?php echo $r['alamat'];?> <?php echo $rd['jnp']=='Desa'? "Desa" : "Kelurahan";?> <?php echo $r['kelurahan'];?></td>
+  </tr>
+  <tr>
+    <td></td><td></td><td></td><td> Distrik <?php echo $r['kec'];?> Kabupaten <?php echo $r['kab'];?></td>
+  </tr>
+
+      <tr>
+    <td colspan="4">&nbsp;</td>
+  </tr>
+    <tr>
+    <td colspan="4">Warga tersebut diatas memiliki Tanah yang diperoleh dari <?php echo $dt[4];?> seluas <?php echo format_angka($dt[2]);?> M2 <b><i>(<?php echo ucwords(terbilang($dt[2]));?> Meter Persegi)</i></b> yang terletak di <?php echo $dt[3];?>, dengan batas - batas sebagai berikut :</td>
+  </tr>
+    <tr>
+    <td>1.</td><td>Barat berbatasan dengan</td><td>:</td><td><?php echo $dt[5];?></td>
+  </tr>
+      <tr>
+    <td>2.</td><td>Utara berbatasan dengan</td><td>:</td><td><?php echo $dt[6];?></td>
+  </tr>
+      <tr>
+    <td>3.</td><td>Timur berbatasan dengan</td><td>:</td><td><?php echo $dt[7];?></td>
+  </tr>
+      <tr>
+    <td>4.</td><td>Selatan berbatasan dengan</td><td>:</td><td><?php echo $dt[8];?></td>
+  </tr>
+    <tr>
+    <td colspan="4">&nbsp;</td>
+  </tr>
+    <tr>
+    <td colspan="4">Demikian keterangan ini dibuat, untuk dapat dipergunakan sebagaimana mestinya. </td>
+  </tr>
+
+
+  <tr><td colspan="4">
+  <!-- Container geser ke kanan tapi isi tetap rata kiri -->
+  <div style="width: 40%; float: right; text-align: left;">
+    <div style="font-size: 12pt; line-height: 1.5;">
+      Dikeluarkan di : <?php echo $rd['kelurahan']; ?><br>
+      Pada Tanggal &nbsp;&nbsp;: <?php echo tgl_indonesia($tgl_sekarang); ?>
+    </div>
+
+    <div style="font-weight: bold; font-size: 12pt; margin-top: 5px;">
+      KEPALA KAMPUNG
+    </div>
+
+    <?php 
+    $queryrs = mysqli_query($con, "SELECT * FROM setting_surat LIMIT 1");
+    while ($rs = mysqli_fetch_array($queryrs)) {
+      if ($rs['ttd'] == 'Otomatis'):
+    ?>
+      <!-- Cap dan ttd -->
+      <div style="margin-top: 5px; display: flex; align-items: center; gap: 10px;">
+        <img src="../file/<?php echo $rd['stample']; ?>" style="width: 90px; height: 90px; opacity: 0.9;">
+        <img src="../file/ttd/<?php echo $r['ttd_staff']; ?>" style="width: 90px; height: 90px; margin-left: -35px;">
+      </div>
+    <?php endif; } ?>
+
+    <div style="margin-top: 5px;">
+      <u><b><?php echo strtoupper($r['nama_staff']); ?></b></u><br>
+      NIP. <?php echo !empty($rd['niplurah']) ? $rd['niplurah'] : '-'; ?>
+    </div>
+  </div>
+</td></tr>
 </table>
-
   <?php }} ?>
 </body>
+
 </html>
