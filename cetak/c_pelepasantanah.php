5dd0a402687e5f3f99096b346284c6a2c139eabd
LuciaSorontou <147905628+LuciaSorontou@users.noreply.github.com>
2025-07-29 22:41:15 +0700

    Update c_pelepasantanah.php

--- cetak/c_pelepasantanah.php
+++ cetak/c_pelepasantanah.php
@@ -2,184 +2,142 @@
 include_once "../koneksi.php";
 include_once "../assets/inc.php";
 
-# Baca variabel URL
 $kodesurat = $_GET['kode'];
-# Perintah untuk mendapatkan data dari tabel Surat 
-$query = mysqli_query ($con, "SELECT tb_jenissurat.*, tb_datasurat.*, tb_detailsurat.*, tb_penduduk.* from tb_jenissurat, tb_datasurat, tb_detailsurat, tb_penduduk WHERE tb_detailsurat.kode='$kodesurat' AND tb_detailsurat.nik=tb_penduduk.nik");
-while ($r = mysqli_fetch_array($query)){
-  $dt=explode(';',$r['detail']);
-  $tgl = $r['tanggal'];
-  $bl=format_hari_tanggal($tgl);
-  $bln=explode(',',$bl);
-  $bulan=$bln['1'];
-  $hari=$bln['0'];
-?>
-<?php 
-$query = mysqli_query ($con, "SELECT * from tb_kelurahan");
-while ($rd = mysqli_fetch_array($query)){
+$query = mysqli_query($con, "SELECT * FROM tb_detailsurat 
+  JOIN tb_staff ON tb_detailsurat.ttd = tb_staff.id_staff 
+  LEFT JOIN tb_penduduk ON tb_detailsurat.nik = tb_penduduk.nik 
+  WHERE tb_detailsurat.kode = '$kodesurat'");
+
+while ($r = mysqli_fetch_array($query)) {
+  $dt = explode(';', $r['detail']);
+  $tgl_sekarang = date('Y-m-d');
+  $hari = date('l');
+
+  function tgl_indonesia($tgl) {
+    $bulan = [
+      '01' => 'Januari', '02' => 'Februari', '03' => 'Maret',
+      '04' => 'April', '05' => 'Mei', '06' => 'Juni',
+      '07' => 'Juli', '08' => 'Agustus', '09' => 'September',
+      '10' => 'Oktober', '11' => 'November', '12' => 'Desember'
+    ];
+    $exp = explode('-', $tgl);
+    return $exp[2] . ' ' . $bulan[$exp[1]] . ' ' . $exp[0];
+  }
+
+  $query_kel = mysqli_query($con, "SELECT * FROM tb_kelurahan");
+  while ($rd = mysqli_fetch_array($query_kel)) {
 ?>
 
-<body onLoad="window.print()" >
-<h1 align="center">
-<table width="800" align="center" border="0" cellspacing="1" cellpadding="4" class="table-print">
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Surat Pernyataan Gadai Tanah</title>
+</head>
+<body onload="window.print()">
+
+<table width="100%" style="text-align:center;">
   <tr>
-    <td rowspan="3" width="70"><img src="../img/<?php echo $rd['logo'];?>" width="60" height="60"></td>
-    <td colspan="" align="center"><strong><font size=2 color="black">PEMERINTAH KABUPATEN <?php echo strtoupper($rd['kab']);?></font></a>
-    </strong></td><td></td>
+    <td rowspan="4" width="80"><img src="../img/<?php echo $rd['logo']; ?>" width="70"></td>
+    <td><strong style="font-size:18px;">PEMERINTAH KABUPATEN <?php echo strtoupper($rd['kab']); ?></strong></td>
   </tr>
-    <tr>
-    <td colspan="" align="center"><strong><font size=3 color="black">KECAMATAN <?php echo strtoupper($rd['kec']);?></font></a>
-    </strong></td><td width="70"></td>
-  </tr>
-    <tr>
-    <td colspan="" align="center"><strong><font size=5 color="black"><?php echo strtoupper($rd['jnp']);?> <?php echo strtoupper($rd['kelurahan']);?></font></strong></td>
-    <td width="70"></td>
-  </tr>
-    <tr>
-   <td colspan="3" align="center"><hr><font size=2 color="black"><i>Sekretariat : <?php echo $rd['kantor'];?></i><hr size="3"></td>
-  </tr>
-    <tr>
-    <td colspan="3" align="center"><strong><font size=4 color="black"><u><?php echo strtoupper($r['nmsurat']); ?> </u></font></a>
-    </strong><br><font size=2 color="black">Nomor : <?php echo $r['no']; ?></font></td>
-  </tr>
+  <tr><td><strong style="font-size:16px;">DISTRIK <?php echo strtoupper($rd['kec']); ?></strong></td></tr>
+  <tr><td><strong style="font-size:20px;">KAMPUNG <?php echo strtoupper($rd['kelurahan']); ?></strong></td></tr>
+  <tr><td><small><i><?php echo $rd['kantor']; ?></i></small></td></tr>
 </table>
+
+<hr style="border: 2px double black; margin-top: 10px; margin-bottom: 20px;">
+
+<center>
+  <h4><u><?php echo strtoupper($r['nmsurat']); ?></u></h4>
+  <p>Nomor: <?php echo $r['no']; ?></p>
+</center>
+
+<p style="text-align:justify;">
+  Yang bertanda tangan di bawah ini <?php echo $rd['jnp'] == 'Desa' ? 'Kepala Kampung' : 'Lurah'; ?> <?php echo $rd['kelurahan']; ?>, Distrik <?php echo $rd['kec']; ?>, Kabupaten <?php echo $rd['kab']; ?>, menerangkan bahwa:
+</p>
+
+<!-- Data Pihak I -->
+<b>PIHAK I:</b>
+<table width="100%">
+  <tr><td width="30%">Nama</td><td>: <?php echo $dt[1]; ?></td></tr>
+  <tr><td>NIK</td><td>: <?php echo $dt[0]; ?></td></tr>
+  <tr><td>Jenis Kelamin</td><td>: <?php echo $dt[2]; ?></td></tr>
+  <tr><td>Tempat/Tgl Lahir</td><td>: <?php echo $dt[3]; ?>, <?php echo $dt[4]; ?></td></tr>
+  <tr><td>Alamat</td><td>: <?php echo $dt[5]; ?></td></tr>
+</table>
+
 <br>
-<table align="center" class="table-list" width="90%" border="0" cellspacing="0" cellpadding="1">
-  <tr>
-    <td colspan="5" align="justify">Yang bertanda tangan dibawah ini <?php echo $rd['jnp']=='Desa'? "Kepala Desa" : "Lurah";?> <?php echo $rd['kelurahan'];?> Kecamatan <?php echo $rd['kec'];?> Kabupaten <?php echo $rd['kab'];?>, menerangkan sebenar - benarnya bahwa telah terjadi Pelepasan Hak Milik atas Tanah dari Pihak I kepada pihak II sebagai berikut : </td>
-  </tr>
-      <tr>
-    <td colspan="5">&nbsp;</td>
-  </tr>
-  <tr>
-    <td>I.</td><td width="30%">Nama</td><td width="2%">:</td><td colspan="2"><b><?php echo $dt[1];?></b></td>
-  </tr>
-  <tr>
-    <td></td><td>NIK</td><td>:</td><td colspan="2"><?php echo $dt[0];?></td>
-  </tr>
-    <tr>
-    <td></td><td>Jenis Kelamin</td><td>:</td><td colspan="2"><?php echo $dt[2];?></td>
-  </tr>
-      <tr>
-    <td></td><td>Tmp. & Tgl. Lahir</td><td>:</td><td colspan="2"><?php echo $dt[3];?>, <?php echo $dt[4];?></td>
-  </tr>
 
-  <tr>
-    <td></td><td>Alamat</td><td>:</td><td colspan="2"><?php echo $dt[5];?></td>
-  </tr>
-  <tr>
-    <td></td><td>Selanjutnya disebut selaku</td><td>:</td><td colspan="2">Pihak I</td>
-  </tr>
-    <tr>
-    <td></td><td>&nbsp;</td><td></td><td colspan="2"></td>
-  </tr>
-  <tr>
-    <td>II.</td><td width="30%">Nama</td><td>:</td><td colspan="2"><b><?php echo $dt[7];?></b></td>
-  </tr>
-  <tr>
-    <td></td><td>NIK</td><td>:</td><td colspan="2"><?php echo $dt[6];?></td>
-  </tr>
-    <tr>
-    <td></td><td>Jenis Kelamin</td><td>:</td><td colspan="2"><?php echo $dt[8];?></td>
-  </tr>
-      <tr>
-    <td></td><td>Tmp. & Tgl. Lahir</td><td>:</td><td colspan="2"><?php echo $dt[9];?>, <?php echo $dt[10];?></td>
-  </tr>
-
-  <tr>
-    <td></td><td>Alamat</td><td>:</td><td colspan="2"><?php echo $dt[11];?> </td>
-  </tr>
-  <tr>
-    <td></td><td>Selanjutnya disebut selaku</td><td>:</td><td colspan="2">Pihak II</td>
-  </tr>
-  <tr>
-    <td colspan="5">&nbsp;</td>
-  </tr>
-  <tr>
-    <td colspan="5" align="justify">Adapun Pelepasan Hak Milik atas Tanah dimaksud terjadi dikarenakan pada hari <?php echo strtoupper($hari);?> tanggal <?php echo IndonesiaTgl($tgl);?> Pihak I telah menjual sebidang tanah dengan luas/ukuran <?php echo format_angka($dt[12]);?> Ha./M2 yang terletak di <?php echo $dt[13];?> kepada Pihak II dengan harga <b>Rp. <?php echo format_angka($dt[14]);?></b>, <i>(<?php echo kekata($dt[14]);?> Rupiah)</i>, dengan batas - batas tanah : </td>
-  </tr>
-
-  <tr>
-    <td></td><td> - Barat berbatasan dengan</td><td>:</td><td colspan="2"><?php echo $dt[15];?></td>
-  </tr>
-      <tr>
-    <td></td><td> - Utara berbatasan dengan</td><td>:</td><td colspan="2"><?php echo $dt[16];?></td>
-  </tr>
-      <tr>
-    <td></td><td> - Timur berbatasan dengan</td><td>:</td><td colspan="2"><?php echo $dt[17];?></td>
-  </tr>
-      <tr>
-    <td></td><td> - Selatan berbatasan dengan</td><td>:</td><td colspan="2"><?php echo $dt[18];?></td>
-  </tr>
-
-  <tr>
-    <td colspan="5">&nbsp;</td>
-  </tr>
-    <tr>
-    <td colspan="5" align="justify" align="justify">Demikian keterangan ini dibuat dengan sebenar - benarnya untuk dipergunakan sebagaimana mestinya.</td>
-  </tr>
-
- <tr>
-    <td></td><td align="center"><br>Pihak II<br>Membeli </td><td><td></td><td align="center"><?php echo $rd['kelurahan'];?>,&nbsp;<?php echo $bulan;?><br>Pihak I<br>Yang Menjual</td>
-  </tr>
-     <tr>
-    <td colspan="5">&nbsp;<br></td><td></td>
-  </tr>
-   <tr>
-    <td colspan="4">&nbsp;<br></td><td><small style="color: grey;">Materai</small></td>
-  </tr>
-     <tr>
-    <td colspan="5">&nbsp;<br></td><td></td>
-  </tr>
-<tr>
-    <td></td><td align="center"><b><u><?php echo $dt[7];?></u></b></td><td></td><td></td><td align="center"><b><u><?php echo $dt[1];?></u></b></td>
-  </tr>
-   <tr>
-    <td colspan="4">Saksi - saksi :</td><td></td>
-  </tr>
-   <tr>
-    <td>1.</td><td><?php echo $dt[19];?></td><td colspan="2">(_______________)</td>
-  </tr>
-   <tr>
-    <td colspan="4">&nbsp;<br></td><td></td>
-  </tr>
-   <tr>
-    <td>2.</td><td><?php echo $dt[20];?></td><td colspan="2">(_______________)</td>
-  </tr>
-     <tr>
-    <td colspan="4">&nbsp;<br></td><td></td>
-  </tr>
-  <tr>
-    <td>3.</td><td><?php echo $dt[21];?></td><td colspan="2">(_______________)</td>
-  </tr>
-     <tr>
-    <td colspan="4">&nbsp;<br></td><td></td>
-  </tr>
-  <tr>
-    <td>4.</td><td><?php echo $dt[22];?></td><td colspan="2">(_______________)</td>
-  </tr>
-     <tr>
-    <td colspan="4">&nbsp;<br></td><td></td>
-  </tr>
-<tr><td colspan="4">
-<table width="90%" align="right" border="0" cellspacing="1" cellpadding="2" class="table-print">
-  <tr>
-    <td></td><td align="center" class="pull pull-right" colspan="2">Mengetahui</td>
-  </tr>
-  <tr>
-    <td rowspan="3" width="30%"></td><td align="center" valign="top" class="pull pull-right"><?php echo $rd['jnp']=='Desa'? "Kepala Desa" : "Lurah";?> <?php echo $rd['kelurahan'];?></td>
-  </tr>
-  <tr>
-    <td align="center" class="pull pull-right"></td>
-  </tr>
-  <tr>
-    <td align="center" class="pull pull-right" width="45%" colspan="2"><br><br><br><u><b><?php echo $r['ttd'];?></b></u><br>NIP. <?php echo $rd['niplurah'];?></td>
-  </tr> 
+<!-- Data Pihak II -->
+<b>PIHAK II:</b>
+<table width="100%">
+  <tr><td width="30%">Nama</td><td>: <?php echo $dt[7]; ?></td></tr>
+  <tr><td>NIK</td><td>: <?php echo $dt[6]; ?></td></tr>
+  <tr><td>Jenis Kelamin</td><td>: <?php echo $dt[8]; ?></td></tr>
+  <tr><td>Tempat/Tgl Lahir</td><td>: <?php echo $dt[9]; ?>, <?php echo $dt[10]; ?></td></tr>
+  <tr><td>Alamat</td><td>: <?php echo $dt[11]; ?></td></tr>
 </table>
-</td>
-</tr>
+
+<br>
+
+<p style="text-align:justify;">
+Telah terjadi gadai tanah dari PIHAK I kepada PIHAK II yang terletak di <?php echo $dt[13]; ?> seluas <?php echo $dt[12]; ?> m², dengan harga Rp. <?php echo format_angka($dt[14]); ?> (<?php echo kekata($dt[14]); ?> Rupiah).
+</p>
+
+<p>Batas-batas tanah sebagai berikut:</p>
+<ul>
+  <li>Barat: <?php echo $dt[15]; ?></li>
+  <li>Utara: <?php echo $dt[16]; ?></li>
+  <li>Timur: <?php echo $dt[17]; ?></li>
+  <li>Selatan: <?php echo $dt[18]; ?></li>
+</ul>
+
+<p style="text-align:justify;">Demikian surat pernyataan ini dibuat dengan sebenar-benarnya untuk dipergunakan sebagaimana mestinya.</p>
+
+<br><br>
+
+<table width="100%">
+  <tr>
+    <td align="center">Pihak II<br><br><br><br><u><?php echo $dt[7]; ?></u></td>
+    <td align="center"><?php echo $rd['kelurahan']; ?>, <?php echo tgl_indonesia($tgl_sekarang); ?><br>Pihak I<br><br><br><br><u><?php echo $dt[1]; ?></u></td>
+  </tr>
 </table>
-  <?php }} ?>
+
+<br><br>
+
+<b>Saksi-saksi:</b>
+<ol>
+  <li><?php echo $dt[19]; ?> (_______________)</li>
+  <li><?php echo $dt[20]; ?> (_______________)</li>
+  <li><?php echo $dt[21]; ?> (_______________)</li>
+  <li><?php echo $dt[22]; ?> (_______________)</li>
+</ol>
+
+<br><br>
+
+<!-- TTD Kepala -->
+<div style="width: 40%; float: right; text-align: left;">
+  <p>Dikeluarkan di: <?php echo $rd['kelurahan']; ?><br>
+  Pada Tanggal: <?php echo tgl_indonesia($tgl_sekarang); ?></p>
+
+  <p><b>KEPALA KAMPUNG</b></p>
+
+  <?php 
+    $queryrs = mysqli_query($con, "SELECT * FROM setting_surat LIMIT 1");
+    while ($rs = mysqli_fetch_array($queryrs)) {
+      if ($rs['ttd'] == 'Otomatis'):
+  ?>
+  <div style="margin-top: 5px;">
+    <img src="../file/<?php echo $rd['stample']; ?>" style="width: 90px;">
+    <img src="../file/ttd/<?php echo $r['ttd_staff']; ?>" style="width: 90px; margin-left: -40px;">
+  </div>
+  <?php endif; } ?>
+
+  <p><u><b><?php echo strtoupper($r['nama_staff']); ?></b></u><br>
+  NIP. <?php echo !empty($rd['niplurah']) ? $rd['niplurah'] : '-'; ?></p>
+</div>
+
+<?php } } ?>
+
 </body>
-
 </html>
