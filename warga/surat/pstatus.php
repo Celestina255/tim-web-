5696b9cfb879efdbde09ce6988873d6b1d0a271c
novensa <florentademano@gmail.com>
2025-07-31 00:13:09 +0900

    Update pstatus.php

--- warga/surat/pstatus.php
+++ warga/surat/pstatus.php
@@ -1,232 +1,139 @@
 <?php
-include_once "../assets/inc.php";
+include_once "../koneksi.php"; // koneksi database
+include_once "../assets/inc.php"; // styling dan header
+session_start();
 ?>
+
 <div class="container mt-4">
-    <div class="row">
-        <div class="col-md-9">
-            <h3 class="title-5">SURAT PERNYATAAN STATUS
-            </h3>
+    <h3 class="text-center mb-4">STATUS SURAT</h3>
+
+    <!-- ======= STATUS SURAT PERMOHONAN (PINDAH KE ATAS) ======= -->
+    <div class="card mb-4">
+        <div class="card-header bg-success text-white">
+            <h5 class="mb-0"><i class="fa fa-envelope"></i> STATUS SURAT PERMOHONAN</h5>
         </div>
-        <div class="col-md-3">
-            <a href="?page=surat_mandiri" type="button" class="btn btn-primary">Kembali</a>
-        </div>
+        <div class="card-body">
+            <table id="table-permohonan" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Surat</th>
+                        <th>Tanggal</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+<?php
+$no = 1;
+$userid = $_SESSION['userid'];
 
-    </div>
-    <hr class="line-seprate">
+$query = mysqli_query($con, "
+    SELECT b.*, j.nmsurat, p.nama 
+    FROM tb_buatsendiri b
+    JOIN tb_jenissurat j ON j.kode = b.kode_jenis
+    JOIN tb_penduduk p ON p.nik = b.nik
+    WHERE b.userid = '$userid' AND b.status != 'acc'
+    ORDER BY b.id DESC
+");
 
-    <div class="card-body card-block" style="box-shadow: 4px 5px 4px;">
-        <?php
-        $query = mysqli_query($con, "SELECT max(kode) as kodeTerbesar FROM tb_datasurat");
-        $data = mysqli_fetch_array($query);
-        $kodesurat = $data['kodeTerbesar'];
+if (mysqli_num_rows($query) == 0) {
+    echo '<tr><td colspan="5" class="text-center">Belum ada data</td></tr>';
+} else {
+    while ($data = mysqli_fetch_assoc($query)) {
+        if ($data['status'] == 'ditolak') {
+            $status_display = "Surat Ditolak";
+        } else {
+            $status_display = "Surat Sedang Diproses";
+        }
 
-                                // mengambil angka dari kode barang terbesar, menggunakan fungsi substr
-                                // dan diubah ke integer dengan (int)
-        $urutan = (int) substr($kodesurat, 2, 3);
-
-                                // bilangan yang diambil ini ditambah 1 untuk menentukan nomor urut berikutnya
-        $urutan++;
-
-                                // membentuk kode surat baru
-                                // perintah sprintf("%03s", $urutan); berguna untuk membuat string menjadi 3 karakter
-                                // misalnya perintah sprintf("%03s", 15); maka akan menghasilkan '015'
-                                // angka yang diambil tadi digabungkan dengan kode huruf yang kita inginkan, misalnya BRG 
-        $huruf = "SR";
-        $kodesurat = $huruf . sprintf("%03s", $urutan);
-        $nosurat = sprintf("%03s", $urutan);
-        ?>
-
-        <form action="act/s_pstatus.php" onsubmit="return validasi_input(this)" method="post" enctype="multipart/form-data" class="form-horizontal">
-
-            <div class="row form-group">
-                <div class="col col-md-3"><label for="kodesurat" class="form-control-label">Kode Surat</label></div>
-                <div class="col-12 col-md-3"><input type="text" id="kodesurat" name="kodesurat" value="<?php echo $kodesurat ?>" readonly="readonly" class="form-control"></div>
-            </div>
-            <div class="row form-group">
-                <div class="col col-md-3"><label for="nmsurat" class=" form-control-label">Jenis / Nama Surat</label></div>
-                <div class="col-12 col-md-6">
-                    <select name="nmsurat" id="nmsurat" onchange="isiredaksi()" class="form-control">
-                        <option value="">Pilih Jenis atau Nama Surat</option>
-                        <?php 
-                        $query = mysqli_query ($con, "SELECT * FROM tb_jenissurat WHERE kode='JS098' ORDER BY id ASC ");
-                        while ($r = mysqli_fetch_array($query)){
-                            ?>
-                            <option value="<?php echo $r['nmsurat'];?>"><?php echo $r['kode'];?>--><?php echo $r['nmsurat'];?></option>
-
-                        </select>
-                    </div>
-                </div>
-                <?php
-                $query = mysqli_query ($con, "SELECT * FROM tb_kelurahan WHERE id='1'");
-                while ($rdes = mysqli_fetch_array($query)){
-                    ?>
-
-
-                    <div class="row form-group">
-                        <div class="col-12 col-md-9"><input type="hidden" id="nosurat" name="nosurat" class="form-control" value="<?php echo $nosurat ?>"></div>
-                        <?php $idstf=$_SESSION['userid']; ?>
-                        <input type="hidden" id="idstf" name="idstf" value="<?php echo $idstf; ?>">
-                        <input type="hidden" id="kode_klasi" name="kode_klasi">
-                        <input type="hidden" id="lurah" name="lurah" value="<?php echo $rdes['lurah']; ?>" >
-                        <input type="hidden" id="kodejenis" name="kodejenis" value="<?php echo $r['kode']; ?>" >
-
-                    </div>
-                <?php }}?>
-                <h6>IDENTITAS PEMBUAT PERNYATAAN :</h6>
-                <hr>
-                <div class="row form-group">
-                    <div class="col col-md-3"><label for="nik" class=" form-control-label">NIK</label></div>
-                    <div class="col-12 col-md-4"><input type="text" id="nik" name="nik" onkeyup="isinik()" class="form-control" maxlength="16"  placeholder="NIK" ></div>
-                </div>
-                <div class="row form-group">
-                    <div class="col col-md-3"><label for="nama" class=" form-control-label">Nama</label></div>
-                    <div class="col-12 col-md-6"><input type="text" id="nama" name="nama" placeholder="Nama Lengkap" class="form-control"></div>
-                </div>
-
-
-                <div class="modal-footer">
-                    <div class="col col-md-6"><button type="reset" class="btn btn-primary btn-sm pull pull-left">Reset</button></div>
-                    <div class="col col-md-6"><button type="submit" name="cetak" class="btn btn-primary btn-sm pull pull-right">Cetak</button></div>
-
-                </div>
-            </div>
-        </form>
-
-
-    <div class="content mt-3">
-        <div class="animated fadeIn">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <strong class="card-title">Data Surat Pernyataan Status</strong>
-                        </div>
-
-                        <div class="card-body scroll">
-                            <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Kode Surat</th>
-                                        <th>Tanggal</th>
-                                        <th>Nama Pembuat Pernyataan</th>
-                                        <th>Alamat</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    <?php 
-                                    $userid=$_SESSION['userid'];
-                                    $query = mysqli_query ($con, "SELECT * FROM tb_buatsendiri, tb_detailsurat JOIN tb_penduduk ON tb_penduduk.nik=tb_detailsurat.nik WHERE tb_detailsurat.kode=tb_buatsendiri.kode_surat AND tb_detailsurat.kodejenis='JS098' AND tb_buatsendiri.status='acc' AND tb_buatsendiri.userid='$userid' ORDER BY tb_detailsurat.id DESC ");
-                                    $no=1;
-                                    while ($data = mysqli_fetch_assoc($query)){
-                                       ?>
-                                       <tr>
-                                        <td><?php echo $no++;?></td>
-                                        <td><?php echo $data['kode'];?></td>
-                                        <td><?php echo $data['tanggal'];?></td>
-                                        <td><?php echo $data['nama'];?></td>
-                                        <td><?php echo $data['alamat'];?></td>
-                                        <td>
-                                            <a href="../cetak/c_pstatus.php?kode=<?php echo $data['kode'];?>" target="_BLANK" class="btn"><i class="fa fa-print"></i></a>
-                                            <a href="?page=edit_pstatus&amp;kode=<?php echo "$data[kode]"; ?>" class="btn"><i class="fa fa-edit"></i></a>
-                                            <a href="hapus/h_pstatus.php?kode=<?php echo $data['kode'];?>" class="btn"><i class="fa fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-
-                                <?php }?>    
-
-                            </tbody>
-                        </table>
-                    </div>      
-                </div>
-            </div>
-        </div>
-
-    </div><!-- .animated -->
-</div>
-</div><!-- .content -->
-<br><br>
-<!-- jQuery 3 -->
-<script src="../assets/js/jquery.min.js"></script> <!-- untuk Pemanggilan data penduduk -->
-
-<script type="text/javascript">
-    $(document).ready(function() {
-      $('#bootstrap-data-table-export').DataTable();
-  } );
-</script>
-
-<script>
-    jQuery(document).ready(function() {
-        jQuery(".standardSelect").chosen({
-            disable_search_threshold: 10,
-            no_results_text: "Oops, nothing found!",
-            width: "100%"
-        });
-    });
-</script>
-
-<script type="text/javascript">
-    function isinik(){
-        var nik = $("#nik").val();
-        $.ajax({
-            url: '../include/ambil_datawarga.php',
-            data:"nik="+nik ,
-        }).success(function (data) {
-            var json = data,
-            obj = JSON.parse(json);
-            $('#nama').val(obj.nama);
-            $('#jk').val(obj.jk);
-            $('#tmp_lahir').val(obj.tmp_lahir);
-            $('#tgl_lahir').val(obj.tgl_lahir);
-            $('#kwng').val(obj.kwng);
-            $('#agama').val(obj.agama);
-            $('#pkjn').val(obj.kerjaan);
-            $('#status').val(obj.status);
-            $('#pend').val(obj.pend);
-            $('#kerjaan').val(obj.kerjaan);
-            $('#almt').val(obj.alamat);
-        });
+        echo '<tr>
+            <td>'.$no++.'</td>
+            <td>'.$data['nama'].'</td>
+            <td>'.$data['nmsurat'].'</td>
+            <td>'.date("d-m-Y", strtotime($data['tgl'])).'</td>
+            <td>'.$status_display.'</td>
+        </tr>';
     }
-</script>
-
-
-<script type="text/javascript">
-    function isiredaksi(){
-        var nmsurat = $("#nmsurat").val();
-        $.ajax({
-            url: '../include/ambil_jenissurat.php',
-            data:"nmsurat="+nmsurat ,
-        }).success(function (data) {
-            var json = data,
-            obj = JSON.parse(json);
-            $('#kode_klasi').val(obj.kode_klasi);
-        });
-    }
-</script>
-
-<script type="text/javascript">
-    function validasi_input(form){
-      var mincar = 16;
-      var minruf = 3;
-      if (form.nmsurat.value=="") {
-          alert('Jenis Surat harus dipilih!');
-          form.nmsurat.focus();
-          return false;
-      }else if(form.nosurat.value=="") {
-          alert('Nomor Surat harus di isi !');
-          form.nosurat.focus();
-          return false;
-      }else if (form.nik.value.length < mincar){
-        alert("Panjang NIK Minimal 16 Karater!");
-        form.nik.focus();
-        return (false);
-    }else if (form.nosurat.value.length < minruf){
-        alert("Panjang Nomor Surat Minimal 3 Angka!");
-        form.nosurat.focus();
-        return (false);
-    }
-    return (true);
 }
+?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <!-- ======= STATUS SURAT MANDIRI (PINDAH KE BAWAH) ======= -->
+    <div class="card mb-4">
+        <div class="card-header bg-success text-white">
+            <h5 class="mb-0"><i class="fa fa-envelope"></i> STATUS SURAT MANDIRI</h5>
+        </div>
+        <div class="card-body">
+            <table id="table-mandiri" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Surat</th>
+                        <th>Tanggal</th>
+                        <th>Status</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+<?php
+$no = 1;
+
+$query = mysqli_query($con, "
+    SELECT b.*, j.nmsurat, p.nama 
+    FROM tb_buatsendiri b
+    JOIN tb_jenissurat j ON j.kode = b.kode_jenis
+    JOIN tb_penduduk p ON p.nik = b.nik
+    WHERE b.userid = '$userid'
+    ORDER BY b.id DESC
+");
+
+if (mysqli_num_rows($query) == 0) {
+    echo '<tr><td colspan="6" class="text-center">Belum ada data</td></tr>';
+} else {
+    while ($data = mysqli_fetch_assoc($query)) {
+        if ($data['status'] == 'acc') {
+            $status_display = "Surat Diterima";
+        } elseif ($data['status'] == 'ditolak') {
+            $status_display = "Surat Ditolak";
+        } else {
+            $status_display = "Surat Sedang Diproses";
+        }
+
+        echo '<tr>
+            <td>'.$no++.'</td>
+            <td>'.$data['nama'].'</td>
+            <td>'.$data['nmsurat'].'</td>
+            <td>'.date("d-m-Y", strtotime($data['tgl'])).'</td>
+            <td>'.$status_display.'</td>
+            <td class="text-end">';
+
+        if ($data['status'] == 'acc') {
+            echo '<a href="../cetak/c_pstatus.php?kode='.$data['kode_surat'].'" target="_blank" class="btn btn-sm btn-primary">
+                    <i class="fa fa-print"></i> Cetak</a>';
+        } else {
+            echo '<span class="text-muted">-</span>';
+        }
+
+        echo '</td></tr>';
+    }
+}
+?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<!-- SCRIPT DATATABLE -->
+<script src="../assets/js/jquery.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#table-mandiri').DataTable();
+        $('#table-permohonan').DataTable();
+    });
 </script>
