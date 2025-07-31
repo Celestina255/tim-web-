8aa6f70afaac74d835648edc145adb605dc65049
oktavianurrosita <148012759+oktavianurrosita@users.noreply.github.com>
2025-07-31 13:17:03 +0700

    Update galeri.php

--- warga/views/galeri.php
+++ warga/views/galeri.php
@@ -1,27 +1,34 @@
+      
       <section id="main-container" class="main-container">
          <div class="container">
             <div class="row text-center">
                <div class="col-md-12">
-                  <h2 class="section-title">Our Gallery</h2>
+                  <h2 class="section-title">GALERI KAMPUNG</h2>
                </div>
             </div>
-            <div class="row">
-             <?php 
-             $query = mysqli_query ($con, "SELECT * FROM tb_galeri ORDER BY id ASC");
-             while ($r = mysqli_fetch_array($query)){
-               ?>
-               <div class="col-lg-3 col-md-6">
-                  <div class="gallery-img">
-                     <a class="gallery-popup" href="../img/galeri/<?php echo $r['foto']; ?>">
-                        <img class="img-fluid" src="../img/galeri/kecil_<?php echo $r['foto']; ?>" alt="" style="width: 350px; box-shadow: 1px 2px 4px;">
-                        <span class="gallery-icon"><i class="fa fa-search"></i></span>
-                     </a>
-                  </div>
-                     <div class="gap-30"></div>
-               </div>
-            <?php } ?>
+           <div class="row">
+  <?php 
+  $query = mysqli_query($con, "SELECT * FROM tb_galeri ORDER BY id ASC");
+  while ($r = mysqli_fetch_array($query)) {
+  ?>
+  <div class="col-lg-3 col-md-6 mb-4 d-flex align-items-stretch">
+    <div class="gallery-img position-relative w-100">
+      <a class="gallery-popup d-block" href="../img/galeri/<?php echo $r['foto']; ?>">
+        <img class="img-fluid w-100 rounded shadow-sm" 
+             src="../img/galeri/kecil_<?php echo $r['foto']; ?>" 
+             alt="Foto Galeri" 
+             style="height: 220px; object-fit: cover; box-shadow: 1px 2px 4px;">
+        <span class="gallery-icon position-absolute top-50 start-50 translate-middle text-white fs-4">
+          <i class="fa fa-search"></i>
+        </span>
+      </a>
+    </div>
+  </div>
+  <?php } ?>
+</div>
+           
             <!--  item 1 end -->
          </div><!--/ Content row 1 end -->
-
-      </div><!--/ Container end -->
-   </section><!-- Main container end -->
+         </div><!--/ Container end -->
+      </section><!-- Main container end -->
+      
