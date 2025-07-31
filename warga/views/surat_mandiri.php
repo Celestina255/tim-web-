49d34e3e5337bcf78cd700596a5d7404561b0c36
novensa <florentademano@gmail.com>
2025-07-31 20:19:24 +0900

    Update surat_mandiri.php

--- warga/views/surat_mandiri.php
+++ warga/views/surat_mandiri.php
@@ -1,8 +1,16 @@
+<?php
+include '../koneksi.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['page']) && $_POST['page'] != '') {
+   $target = $_POST['page'];
+   echo "<script>window.location.href='index.php?page=$target';</script>";
+}
+?>
+
 <section class="main-container contact-area" id="main-container">
    <div class="gap-10"></div>
    <div class="ts-form form-boxed" id="ts-form">
       <div class="container">
-         <div class="row justify-content-center"> <!-- tambahkan class ini untuk pusat -->
+         <div class="row justify-content-center">
             <div class="col-lg-8 col-md-10">
                <h3 class="column-title text-center mb-4">Silahkan Pilih Nama Surat yang akan kamu buat</h3>
                <div class="contact-submit-box contact-box form-box p-4 shadow-sm rounded bg-white">
