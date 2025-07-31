981363e084027d1b9c08c29aa3b0c12d31f47807
LuciaSorontou <147905628+LuciaSorontou@users.noreply.github.com>
2025-07-31 22:03:17 +0700

    Update c_skck.php

--- cetak/c_skck.php
+++ cetak/c_skck.php
@@ -11,18 +11,27 @@ $query = mysqli_query($con, "SELECT * FROM tb_detailsurat
 
 while ($r = mysqli_fetch_array($query)) {
     $dt = explode(';', $r['detail']);
-    $tgl = $r['tanggal'];
-    $bl = format_hari_tanggal($tgl);
-    $bln = explode(',', $bl);
-    $bulan = trim($bln[1]);
+    $tgl_sekarang = date('Y-m-d');
 
-    $query_kel = mysqli_query($con, "SELECT * FROM tb_kelurahan");
-    while ($rd = mysqli_fetch_array($query_kel)) {
+    function tgl_indonesia($tgl) {
+        $bulan = [
+            '01' => 'Januari', '02' => 'Februari', '03' => 'Maret',
+            '04' => 'April', '05' => 'Mei', '06' => 'Juni',
+            '07' => 'Juli', '08' => 'Agustus', '09' => 'September',
+            '10' => 'Oktober', '11' => 'November', '12' => 'Desember'
+        ];
+        $exp = explode('-', $tgl);
+        return $exp[2] . ' ' . $bulan[$exp[1]] . ' ' . $exp[0];
+    }
+
+    $query2 = mysqli_query($con, "SELECT * from tb_kelurahan");
+    while ($rd = mysqli_fetch_array($query2)) {
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
     <title>Permohonan SKCK</title>
     <style>
         body { font-family: 'Times New Roman', serif; }
@@ -34,109 +43,125 @@ while ($r = mysqli_fetch_array($query)) {
         .cap-ttd .ttd { top: 0; left: 10px; }
     </style>
 </head>
-
 <body onload="window.print()">
-    <!-- KOP SURAT -->
-    <table width="800" align="center" border="0" cellpadding="4">
-        <tr>
-            <td rowspan="3" width="70"><img src="../img/<?php echo $rd['logo']; ?>" width="60" height="60"></td>
-            <td align="center"><strong><font size="2">PEMERINTAH KABUPATEN <?php echo strtoupper($rd['kab']); ?></font></strong></td>
-        </tr>
-        <tr>
-            <td align="center"><strong><font size="3">KECAMATAN <?php echo strtoupper($rd['kec']); ?></font></strong></td>
-        </tr>
-        <tr>
-            <td align="center"><strong><font size="4"><?php echo strtoupper($rd['jnp']); ?> <?php echo strtoupper($rd['kelurahan']); ?></font></strong></td>
-        </tr>
-        <tr>
-            <td colspan="2" align="center">
-                <hr><i>Sekretariat: <?php echo $rd['kantor']; ?></i><hr size="3">
-            </td>
-        </tr>
-    </table>
 
-    <!-- INFORMASI SURAT -->
-    <table width="800" align="center" cellpadding="2">
-        <tr>
-            <td width="20%">Nomor</td><td width="2%">:</td><td><?php echo $r['no']; ?></td>
-            <td align="right"><?php echo $rd['kelurahan']; ?>, <?php echo $bulan; ?></td>
-        </tr>
-        <tr>
-            <td>Lampiran</td><td>:</td><td>1 (satu) berkas</td>
-            <td>Kepada Yth,</td>
-        </tr>
-        <tr>
-            <td>Perihal</td><td>:</td><td><b>Permohonan Penerbitan SKCK</b></td>
-            <td><b><?php echo $dt[8]; ?></b></td>
-        </tr>
-        <tr><td colspan="4"></td></tr>
-        <tr><td colspan="4" align="right"><b><u><?php echo $dt[9]; ?></u></b></td></tr>
-    </table>
+<!-- KOP SURAT -->
+<table width="800" align="center" cellpadding="4">
+    <tr>
+        <td rowspan="4" width="80" align="center" valign="top">
+            <img src="../img/<?php echo $rd['logo']; ?>" style="max-width: 85px; height: auto;">
+        </td>
+        <td colspan="2" align="center" style="font-size: 25px;"><strong>PEMERINTAH KABUPATEN <?php echo strtoupper($rd['kab']); ?></strong></td>
+    </tr>
+    <tr>
+        <td colspan="2" align="center" style="font-size: 18px;"><strong>DISTRIK <?php echo strtoupper($rd['kec']); ?></strong></td>
+    </tr>
+    <tr>
+        <td colspan="2" align="center" style="font-size: 23px;"><strong>KAMPUNG <?php echo strtoupper($rd['kelurahan']); ?></strong></td>
+    </tr>
+    <tr>
+        <td colspan="2" align="center" style="font-size: 15px;">Alamat : <?php echo $rd['kantor']; ?></td>
+    </tr>
+    <tr>
+        <td colspan="3" align="center"><hr style="border: 1.5px double black;"><br></td>
+    </tr>
+</table>
 
-    <br>
+<!-- INFORMASI SURAT -->
+<table width="800" align="center" cellpadding="2">
+    <tr>
+        <td width="50%">
+            <table>
+                <tr>
+                    <td width="30%">Nomor</td>
+                    <td width="2%">:</td>
+                    <td><?php echo $r['no']; ?></td>
+                </tr>
+                <tr>
+                    <td>Lampiran</td>
+                    <td>:</td>
+                    <td>1 (satu) berkas</td>
+                </tr>
+                <tr>
+                    <td>Perihal</td>
+                    <td>:</td>
+                    <td><b>Permohonan Penerbitan SKCK</b></td>
+                </tr>
+            </table>
+        </td>
+        <td width="50%" valign="top">
+    <?php echo $rd['kelurahan']; ?>, <?php echo format_hari_tanggal($tgl_sekarang); ?><br><br>
+    Kepada Yth,<br>
+    <b><?php echo $dt[8]; ?></b><br>
+    <div style="width: 100%; text-align: center; padding-center: 30px;">
+        <b><?php echo $dt[9]; ?></b>
+    </div>
+</td>
 
-    <!-- ISI SURAT -->
-    <table width="800" align="center" cellpadding="2">
-        <tr><td colspan="3">Assalamu’alaikum Wr. Wb.</td></tr>
-        <tr><td colspan="3">Dengan hormat,</td></tr>
-        <tr>
-            <td colspan="3" align="justify">
-                Bersama ini <?php echo $rd['jnp'] == 'Desa' ? 'Kepala Kampung' : 'Lurah'; ?> <?php echo $rd['kelurahan']; ?> Distrik <?php echo $rd['kec']; ?> Kabupaten <?php echo $rd['kab']; ?>, 
-                memohon kepada Bapak/Ibu <?php echo $dt[8]; ?> agar berkenan menerbitkan SKCK untuk warga kami sebagai berikut:
-            </td>
-        </tr>
-        <tr><td width="30%">Nama</td><td width="2%">:</td><td><?php echo $dt[1]; ?></td></tr>
-        <tr><td>NIK</td><td>:</td><td><?php echo $dt[0]; ?></td></tr>
-        <tr><td>Jenis Kelamin</td><td>:</td><td><?php echo $dt[2]; ?></td></tr>
-        <tr><td>Tempat/Tgl Lahir</td><td>:</td><td><?php echo $dt[3]; ?>, <?php echo IndonesiaTgl($dt[4]); ?></td></tr>
-        <tr><td>Pekerjaan</td><td>:</td><td><?php echo $dt[5]; ?></td></tr>
-        <tr><td>Alamat</td><td>:</td><td><?php echo $dt[6]; ?></td></tr>
-        <tr>
-            <td colspan="3" align="justify">
-                SKCK tersebut akan digunakan untuk <?php echo $dt[7]; ?>.
-            </td>
-        </tr>
-        <tr>
-            <td colspan="3" align="justify">
-                Demikian permohonan ini kami sampaikan, atas perhatian dan kerjasamanya kami ucapkan terima kasih.
-            </td>
-        </tr>
-        <tr><td colspan="3">Wassalamu’alaikum Wr. Wb.</td></tr>
-    </table>
+    </tr>
+</table>
 
-    <!-- TANDA TANGAN -->
-    <br><br>
-    <table width="90%" align="right" border="0">
-        <tr>
-            <td align="center"><?php echo $rd['kelurahan']; ?>, <?php echo $bulan; ?></td>
-        </tr>
-        <tr>
-            <td align="center">
-                <?php echo ($r['jab_staff'] == 'Kepala Kelurahan' || $r['jab_staff'] == 'Kepala Desa') ? '' : 'a.n.'; ?>
-                <?php echo $rd['jnp'] == 'Desa' ? 'Kepala Desa' : 'Kepala Kelurahan'; ?> <?php echo $rd['kelurahan']; ?><br>
-                <?php echo ($r['jab_staff'] == 'Kepala Kelurahan' || $r['jab_staff'] == 'Kepala Desa') ? '' : $r['jab_staff']; ?>
-            </td>
-        </tr>
-        <tr><td height="100"></td></tr>
-        <tr>
-            <td align="center">
-                <?php
-                $queryrs = mysqli_query($con, "SELECT * from setting_surat LIMIT 1");
-                while ($rs = mysqli_fetch_array($queryrs)) {
-                    if ($rs['ttd'] == 'Otomatis'): ?>
-                    <div class="cap-ttd">
-                        <img src="../file/<?php echo $rd['stample']; ?>" class="cap">
-                        <img src="../file/ttd/<?php echo $r['ttd_staff']; ?>" class="ttd">
-                    </div>
-                <?php endif; } ?>
-                <div class="ttd-nama">
-                    <b><u><?php echo strtoupper($r['nama_staff']); ?></u></b><br>
-                    NIP. <?php echo !empty($rd['niplurah']) ? $rd['niplurah'] : '-'; ?>
-                </div>
-            </td>
-        </tr>
-    </table>
+<br>
+
+<!-- ISI SURAT -->
+<table width="800" align="center" cellpadding="2">
+    <tr><td colspan="3">Assalamu’alaikum Wr. Wb.</td></tr>
+    <tr><td colspan="3">Dengan hormat,</td></tr>
+    <tr>
+        <td colspan="3" align="justify">
+            Bersama ini <?php echo $rd['jnp'] == 'Desa' ? 'Kepala Kampung' : 'Lurah'; ?> <?php echo $rd['kelurahan']; ?> Distrik <?php echo $rd['kec']; ?> Kabupaten <?php echo $rd['kab']; ?>, 
+            memohon kepada Bapak/Ibu <?php echo $dt[8]; ?> agar berkenan menerbitkan SKCK untuk warga kami sebagai berikut:
+        </td>
+    </tr>
+    <tr><td width="30%">Nama</td><td width="2%">:</td><td><?php echo $dt[1]; ?></td></tr>
+    <tr><td>NIK</td><td>:</td><td><?php echo $dt[0]; ?></td></tr>
+    <tr><td>Jenis Kelamin</td><td>:</td><td><?php echo $dt[2]; ?></td></tr>
+    <tr><td>Tempat/Tgl Lahir</td><td>:</td><td><?php echo $dt[3]; ?>, <?php echo IndonesiaTgl($dt[4]); ?></td></tr>
+    <tr><td>Pekerjaan</td><td>:</td><td><?php echo $dt[5]; ?></td></tr>
+    <tr><td>Alamat</td><td>:</td><td><?php echo $dt[6]; ?></td></tr>
+    <tr><td colspan="3" align="justify">SKCK tersebut akan digunakan untuk <?php echo $dt[7]; ?>.</td></tr>
+    <tr><td colspan="3" align="justify">Demikian permohonan ini kami sampaikan, atas perhatian dan kerjasamanya kami ucapkan terima kasih.</td></tr>
+    <tr><td colspan="3">Wassalamu’alaikum Wr. Wb.</td></tr>
+</table>
+
+<br><br>
+
+<!-- TANDA TANGAN -->
+<table width="800" align="center">
+    <tr>
+        <td align="center"><?php echo $rd['kelurahan']; ?>, <?php echo format_hari_tanggal($tgl_sekarang); ?></td>
+    </tr>
+    <tr>
+        <td align="center">
+            <?php echo ($r['jab_staff'] == 'Kepala Kelurahan' || $r['jab_staff'] == 'Kepala Desa') ? '' : 'a.n.'; ?>
+            <?php echo $rd['jnp'] == 'Desa' ? 'Kepala Desa' : 'Kepala Kelurahan'; ?> <?php echo $rd['kelurahan']; ?><br>
+            <?php echo ($r['jab_staff'] == 'Kepala Kelurahan' || $r['jab_staff'] == 'Kepala Desa') ? '' : $r['jab_staff']; ?>
+        </td>
+    </tr>
+    <!-- hilangkan jeda bawah -->
+    <tr><td style="height: 10px;"></td></tr>
+    <tr>
+        <td align="center">
+            <?php
+            $queryrs = mysqli_query($con, "SELECT * from setting_surat LIMIT 1");
+            while ($rs = mysqli_fetch_array($queryrs)) {
+                if ($rs['ttd'] == 'Otomatis') {
+                    echo '<div class="cap-ttd">
+                            <img src="../file/' . $rd['stample'] . '" class="cap">
+                            <img src="../file/ttd/' . $r['ttd_staff'] . '" class="ttd">
+                          </div>';
+                }
+            }
+            ?>
+            <div class="ttd-nama">
+                <b><u><?php echo strtoupper($r['nama_staff']); ?></u></b><br>
+                NIP. <?php echo !empty($rd['niplurah']) ? $rd['niplurah'] : '-'; ?>
+            </div>
+        </td>
+    </tr>
+</table>
 
 </body>
 </html>
+
 <?php } } ?>
