9438636138f99975ed1e9370bfff0ca9f5be1bf2
LuciaSorontou <147905628+LuciaSorontou@users.noreply.github.com>
2025-08-06 22:45:19 +0700

    Update c_suketlain.php

--- cetak/c_suketlain.php
+++ cetak/c_suketlain.php
@@ -21,6 +21,16 @@ while ($r = mysqli_fetch_array($query)) {
       $exp = explode('-', $tgl);
       return $exp[2] . ' ' . $bulan[$exp[1]] . ' ' . $exp[0];
   }
+  function tgl_lahir_indo($tgl) {
+    $bulan = [
+        '01' => 'Januari', '02' => 'Februari', '03' => 'Maret',
+        '04' => 'April', '05' => 'Mei', '06' => 'Juni',
+        '07' => 'Juli', '08' => 'Agustus', '09' => 'September',
+        '10' => 'Oktober', '11' => 'November', '12' => 'Desember'
+    ];
+    $exp = explode('/', $tgl); // format di database: 07/09/1968
+    return (int)$exp[0] . ' ' . $bulan[$exp[1]] . ' ' . $exp[2];
+}
 
   $query = mysqli_query($con, "SELECT * from tb_kelurahan");
   while ($rd = mysqli_fetch_array($query)) {
@@ -74,7 +84,7 @@ while ($r = mysqli_fetch_array($query)) {
     <td>Jenis Kelamin</td><td>:</td><td><?php echo $dt[3];?></td>
   </tr>
   <tr>
-    <td>Tmp. & Tgl. Lahir</td><td>:</td><td><?php echo $dt[4];?>, <?php echo $dt[5];?> </td>
+    <td>Tempat / Tanggal Lahir</td><td>:</td><td><?php echo $dt[4];?>, <?php echo tgl_lahir_indo($dt[5]);?> </td>
   </tr>
     <tr>
     <td>Agama</td><td>:</td><td><?php echo $dt[6];?></td>
