73232f115531ad5d7af707fb12c2540b8daed408
ULINIKEN3020 <135414818+ULINIKEN3020@users.noreply.github.com>
2025-08-09 23:53:02 +0900

    Update hapus_permohonan.php

--- administrator/hapus/hapus_permohonan.php
+++ administrator/hapus/hapus_permohonan.php
@@ -3,9 +3,14 @@ include_once "../../koneksi.php"; // Sesuaikan path ke koneksi
 
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
-    $hapus = mysqli_query($con, "DELETE FROM tb_permohonan WHERE id='$id'");
 
-    if ($hapus) {
+    // Menghapus dari tabel tb_permohonan
+    $hapus_permohonan = mysqli_query($con, "DELETE FROM tb_permohonan WHERE id='$id'");
+
+    // Menghapus dari tabel tb_buatsendiri
+    $hapus_buatsendiri = mysqli_query($con, "DELETE FROM tb_buatsendiri WHERE id='$id'");
+
+    if ($hapus_permohonan || $hapus_buatsendiri) {
         header("Location: ../index.php?page=permohonan"); // redirect
         exit;
     } else {
